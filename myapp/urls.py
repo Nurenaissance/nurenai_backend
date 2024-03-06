1599b72976d5bfe6fa55a495127d8c31f2a5f827
Nurenaissance <154663519+Nurenaissance@users.noreply.github.com>
2024-03-06 17:11:42 +0530

    whatsapp_bot

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from . import views1
 from . import views2
 from . import views3
+from . import views4
 
 urlpatterns = [
     # ... other patterns
     path('upload-pdf/', views1.upload_pdf_view, name='upload-pdf'),
     path('get-pdf/', views2.query_pdf_view, name='get-pdf'),
     path('upload-audio/', views3.upload_audio, name='upload-audio'),
+    path('sms', views4.incoming_sms, name='sms'),
     # ... other patterns
 ]
